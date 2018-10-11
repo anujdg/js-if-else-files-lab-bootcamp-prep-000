fde1e95aff350808c8b38888fbb4bc5f36409645
anuj.kr1324@outlook.com <anuj.vr99@gmail.com>
2018-10-11 13:33:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-var name = "Joe"
+var name = "susan"
 var height = "74"
 
 
 // Don't worry about this
 module.exports = { name, height
 }
+
+if (name === "susan")  {
+  console.log("the name is correct.")
+  
+} else{
+  console.log("expected name is wrong")
+}
+
+if (age === "24")  {
+  console.log("your age is correct")
+} else{
+  console.log("expect age is wrong")
+}
+
 
